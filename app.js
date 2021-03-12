1dfc19a06f00748bc16130b349639c0c2814deaa
Jamie Jones <jbjonesjr@github.com>
2021-03-12 13:51:31 +0000

    working on puppeteer

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 var html_to_pdf = require('html-pdf-node');
+const puppeteer = require('puppeteer-core');
 
 // Use UTC for for all Date parsing
 process.env.TZ = 'UTC'
 
+
 module.exports = robot => {
+  robot.on(['create', 'workflow_dispatch', 'repository_dispatch'], (context) => {
 
-    
-  robot.on(['create','workflow_dispatch','repository_dispatch'], (context)=> {
-      console.log(context.payload)
-      let options = { format: 'A4',path:process.env.GITHUB_WORKSPACE+'/vulnerability-alert.pdf' };
+    let file = { content: "<h1>Welcome to html-pdf-node</h1>" };
 
-        let file = { content: "<h1>Welcome to html-pdf-node</h1>" };
+    const fetcher = puppeteer.createBrowserFetcher({ path: process.env.GITHUB_WORKSPACE });
 
-        html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
-            console.log("PDF Buffer:-", pdfBuffer);
-        });
-  })
+    let outfile = fetcher.download('782078')//TODO need to store and inject this
+      .then(revisionInfo => {
+        return puppeteer.launch({ executablePath: revisionInfo.executablePath })
+          .then(browser => {
+            return browser.newPage()
+              .then(page => {
+                return page.setContent(html)
+                  .then(() => {
+                    let tempfile = page.pdf({ path: file, format: 'A4' })
+                  });
+              })
+              .then(() => {
+                return browser.close();
+              });
+          })
+          .then(() => {
+            return file;
+          });
+      });
+  });
 }
---
+++ test/index.test.js
@@ -0,0 +1,22 @@
+const { Probot, ProbotOctokit } = require('probot')
+const plugin = require('../app')
+
+process.env.TZ = 'UTC'
+process.env.GITHUB_WORKSPACE= '/workspaces/github-security-report-action/'
+let event_payload = JSON.parse(JSON.stringify(require('./fixtures/vuln_alert.create.json')))
+
+let robot = new Probot({
+  'secret': 'foo',
+  githubToken: 'test',
+  // Disable throttling & retrying requests for easier testing
+  Octokit: ProbotOctokit.defaults({
+    retry: { enabled: false },
+    throttle: { enabled: false }
+  }),
+  installation: { id: 13055 }
+})
+
+plugin(robot)
+robot.receive({id:1,name:"repository_dispatch", payload: event_payload}).then(result =>
+    console.log(result));
+    
