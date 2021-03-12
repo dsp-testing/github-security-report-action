b7f841eb9c9bada3b781fb335ecaf04c8b47cdc1
Jamie Jones <jbjonesjr@github.com>
2021-03-12 06:17:25 +0000

    fix variable name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const probotApp = require('./app')
 
 if (process.env.GITHUB_ACTIONS) {
-  require('@probot/adapter-github-actions').run(actionApp).catch((error) => {
+  require('@probot/adapter-github-actions').run(probotApp).catch((error) => {
     console.error(error)
     process.exit(1)
   })
