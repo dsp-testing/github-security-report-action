4bc92a6c2b2e70695785298a5e8e292a47aa88cd
Jamie Jones <jbjonesjr@github.com>
2021-03-12 14:57:53 +0000

    fix output path

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ process.env.TZ = 'UTC'
 module.exports = function (robot) {
   robot.on(['create', 'workflow_dispatch', 'repository_dispatch'], (context) => {
 
-    let html = { content: "<h1>A new Vulnerability has been found in your dependencies</h1><hr>" };
+    context.paylad.alert.affected_package_name
+    let html = { content: `
+    <html>
+    <body>
+    <h1>$A new Vulnerability has been found in your dependencies</h1><hr>
+    <ul>
+      <li>Package name: ${context.paylad.alert.affected_package_name}</li>
+      <li>Affected range: ${context.payload.alert.affected_range}
+    </ul>
+    </body>
+    </html>
+    ` };
 
     const fetcher = puppeteer.createBrowserFetcher({ path: process.env.GITHUB_WORKSPACE });
 
@@ -18,8 +29,8 @@ module.exports = function (robot) {
               .then(page => {
                 return page.setContent(html.content)
                   .then(() => {
-                    console.log('printing pdf', process.env.GITHUB_WORKSPACE + "vuln-output-puppeteer.pdf");
-                    return page.pdf({ path: process.env.GITHUB_WORKSPACE + "vulnerability-alert-report", format: 'A4' })
+                    console.log('printing pdf', process.env.GITHUB_WORKSPACE + "/vulnerability-alert.pdf");
+                    return page.pdf({ path: process.env.GITHUB_WORKSPACE + "/vulnerability-alert.pdf", format: 'A4' })
                       .then(() => {
                         return browser.close();
                       })
