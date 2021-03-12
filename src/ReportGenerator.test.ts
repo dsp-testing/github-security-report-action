468074f97bced00ccfc8f40e1190817a04b55bbb
Jamie Jones <jbjonesjr@github.com>
2021-03-12 05:30:33 +0000

    fixing tests

--- src/ReportGenerator.test.ts
+++ src/ReportGenerator.test.ts
@@ -1,5 +1,6 @@
 import { Octokit } from '@octokit/rest';
 import { expect } from 'chai';
+import Vulnerability from './dependencies/Vulnerability';
 import ReportGenerator from './ReportGenerator';
 import { getGitHubToken, getSampleSarifDirectory, getTestDirectoryFilePath } from './testUtils';
 
@@ -19,12 +20,34 @@ describe('ReportGenerator', function () {
 
   this.timeout(10 * 1000);
 
+  let tempVuln = Object.assign({createdAt:'',dismisser:{login:'', name:''},dismissedAt:'', dismissReason:'',
+  vulnerableManifestFilename:'', vulnerableRequirements:'', vulnerableManifestPath:'', securityVulnerability: { package: {
+    ecosystem: '',
+    name: ''
+  },
+  severity: '',
+  vulnerableVersionRange: ''} ,
+   securityAdvisoryts:'', securityAdvisory:{
+    databaseId: '',
+    id: '',
+    summary: '',
+    severity: '',
+    description: '',
+    ghsaId: '',
+    identifiers: {
+      type: '',
+      value: '',
+    },
+    permalink: '',
+    publishedAt: ''
+   }, id:''},{});
+
   [SIMPLE_TEST_REPOSITORY, PM_AS_JAVA].forEach(config => {
     it(`should generate a report for ${config.repository}`, async () => {
       const generatorConfig = {
         octokit: new Octokit({auth: TOKEN}),
         repository: config.repository,
-
+        vulnAlert:  new Vulnerability(tempVuln),
         sarifReportDirectory: config.sarifReportDir,
         outputDirectory: getTestDirectoryFilePath(config.repository),
 
